831af9ea6edc86a3c75d32a432b5693be951441f
EmmanuellaAplerku <emmaplerks@gmail.com>
2021-12-11 19:39:52 +0000

    server

--- Final Exam/WebsiteDb_Connection.php
+++ Final Exam/WebsiteDb_Connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "server";
+$password = "Final.16";
 $dbname = "websitedb";
 
 //$username = "root";
@@ -14,6 +14,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo $conn;
+echo "Successful";
 
 ?>
